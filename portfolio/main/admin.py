1fd486d046015e94ac1c79cbff783aa8e6f12f09
ArthIJani <arth.jani2002@gmail.com>
2024-05-26 15:10:56 +0530

    Edit1

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Tag, Project, ProjectImage
 
+
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
     extra = 1
+
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ("title", "link")
     inlines = [ProjectImageInline]
     search_fields = ("title", "description")
     list_filter = ("tags",)
+
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ("name",)
